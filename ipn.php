0024ac10dbeba37164e87d1da4ace6626bdbff9a
mariano <mariano1colombo@hotmail.com>
2020-10-01 00:09:24 -0300

    Agregado autoload

--- ipn.php
+++ ipn.php
@@ -26,25 +26,4 @@ switch($_GET["topic"]) {
         $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
         break;
 }
-
-$paid_amount = 0;
-foreach ($merchant_order->payments as $payment) {
-    if ($payment['status'] == 'approved'){
-        $paid_amount += $payment['transaction_amount'];
-    }
-}
-
-// If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
-if($paid_amount >= $merchant_order->total_amount){
-    if (count($merchant_order->shipments)>0) { // The merchant_order has shipments
-        if($merchant_order->shipments[0]->status == "ready_to_ship") {
-            print_r("Totally paid. Print the label and release your item.");
-        }
-    } else { // The merchant_order don't has any shipments
-        print_r("Totally paid. Release your item.");
-    }
-} else {
-    print_r("Not paid yet. Do not release your item.");
-}
-
 ?>
