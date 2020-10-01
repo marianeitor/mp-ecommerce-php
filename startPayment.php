5670348e131a49a65665e1be6650d885889bf7d1
mariano <mariano1colombo@hotmail.com>
2020-09-30 23:07:34 -0300

    Seteado Integrator ID y Display de errores

--- startPayment.php
+++ startPayment.php
@@ -1,9 +1,21 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 
+
+
+
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+
+//Seteo de integrator ID
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
