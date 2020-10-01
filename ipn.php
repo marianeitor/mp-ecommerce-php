923cb17fd6688440cff7775d978d7117f3d077c4
mariano <mariano1colombo@hotmail.com>
2020-10-01 00:07:12 -0300

    Cambios ipn

--- ipn.php
+++ ipn.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
